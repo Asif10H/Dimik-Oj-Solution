66915b19567b53ca6f759dc12396de14159c63e5
Habibullah Asif <29340163+Asif10H@users.noreply.github.com>
2020-12-14 18:37:42 +0600

    Create প্রোগ্রামিং সমস্যা 73 - মনু মিয়া (ISCPC 2018 Preliminary).cpp

---
+++ প্রোগ্রামিং সমস্যা 73 - মনু মিয়া (ISCPC 2018 Preliminary).cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef long long unsigned int ull;
+typedef long double ld;
+typedef vector<int> vr;
+typedef pair<int, int> pr;
+#define pb push_back
+#define ff first
+#define ss second
+#define INF 1999999999
+#define MODA 1000000007
+#define PI 3.1415926535897932384626433
+#define endl "\n"
+//cout << fixed << setprecision(2);
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string s;
+    getline(cin, s);
+    reverse(s.begin(), s.end());
+    cout << s << endl;
+
+    return 0;
+}
